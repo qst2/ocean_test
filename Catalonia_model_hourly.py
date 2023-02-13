2aaa44cab09413db427b6ff7b1d89203f30a5dba
qst2 <68461203+qst2@users.noreply.github.com>
2023-02-13 10:47:12 +0300

    Update and rename Catalonia_Ocean_model_1.py to Catalonia_model_hourly.py

--- Catalonia_model_hourly.py
+++ Catalonia_model_hourly.py
@@ -8,8 +8,12 @@ import numpy as np
 import pandas as pd
 from sklearn import preprocessing
 from sklearn.linear_model import LogisticRegression
-from prophet.plot import plot_plotly, plot_components_plotly
-from prophet import Prophet
+
+try:
+       from prophet.plot import plot_plotly, plot_components_plotly
+       from prophet import Prophet
+except:
+       !pip install prophet
 
 hours=['01h', '02h', '03h', '04h', '05h', '06h', '07h', '08h',
        '09h', '10h', '11h', '12h', '13h', '14h', '15h', '16h', '17h', '18h',
